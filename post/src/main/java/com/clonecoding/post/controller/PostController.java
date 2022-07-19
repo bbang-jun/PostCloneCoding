fb9f6ef80cb8113ae1ae430c72171a5ea41cb0ff
BYEONGJUN KANG <lucid9dawn@gmail.com>
2022-07-20 04:07:41 +0900

    [Complete] make project & make table in mariaDB

---
+++ post/src/main/java/com/clonecoding/post/controller/PostController.java
@@ -0,0 +1,11 @@
+package com.clonecoding.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // 이 클래스가 컨트롤러임을 스프링부트가 인식
+public class PostController {
+
+
+}
